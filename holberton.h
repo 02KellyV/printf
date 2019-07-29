88e581598c3661d5374ae48f1c26825fd8d21581
luiseduardiazc <luis.diaz.car@gmail.com>
2019-07-29 15:46:16 +0000

    add prototypes to holberton.h

--- holberton.h
+++ holberton.h
@@ -8,4 +8,9 @@ int _printf(const char *format, ...);
 char *convert_to(char representation[], unsigned int num, int base);
 int function_manager(char c, va_list arg);
 int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
 #endif
