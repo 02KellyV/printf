bcb25bce30e24f4802fdaf5774b3ec8c2b9c7ddc
02KellyV <900@holbertonschool.com>
2019-07-30 20:05:07 +0000

    valid-ptr

--- case_functions2.c
+++ case_functions2.c
@@ -54,35 +54,33 @@ int print_ptr(va_list arg)
 
 /**
  * print_rot13 - prints a string using rot13
- * @l: list of arguments from _printf
+ * @arg: list of arguments from _printf
  * Return: length of the printed string
  */
-
 int print_rot13(va_list arg)
 {
-        register short i, j;
-        char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
-        char *s = va_arg(arg, char *);
+	register short i, j;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *s = va_arg(arg, char *);
 
-        for (j = 0; s[j]; j++)
-        {
-                if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
-                        _putchar(s[j]);
-                else
-                {
-                        for (i = 0; i <= 52; i++)
-                                if (s[j] == rot13[i])
-                                        _putchar(ROT13[i]);
-                }
-        }
-        return (j);
+	for (j = 0; s[j]; j++)
+	{
+		if (s[j] < 'A' || (s[j] > 'Z' && s[j] < 'a') || s[j] > 'z')
+			_putchar(s[j]);
+		else
+		{
+			for (i = 0; i <= 52; i++)
+				if (s[j] == rot13[i])
+					_putchar(ROT13[i]);
+		}
+	}
+	return (j);
 }
 
 /**
  * print_rev - prints a string in reverse
- * @l: argument from _printf
- * @f: pointer to the struct flags that determines
+ * @arg: argument from _printf
  * if a flag is passed to _printf
  * Return: length of the printed string
  */
